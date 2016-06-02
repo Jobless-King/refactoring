76bd03796275d63e01aa44ea0401a9048129c23c
Ksj7 <lobstar733@gmail.com>
2016-06-02 12:05:59 +0900

    REFACTORING_v2
    필요없는 계층구조 삭제 (AbstractList와 AbstractSet)을 삭제하고 AbstractCollection을
    extends 했다.

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -1,6 +1,6 @@
 package com.nts.cleancode.collections;
 
-public class List extends AbstractList {
+public class List extends AbstractCollection{ /* 문제제기 : 필요없는 추상클래스를 제거 하려 한다. 해결방법 : 상위 클래스와의 의존성을 제거*/
 	private Object[] elements = new Object[10];
 	private int size = 0;
 	private boolean readOnly;
--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -1,6 +1,6 @@
 package com.nts.cleancode.collections;
 
-public class Set extends AbstractSet {
+public class Set extends AbstractCollection{/* 문제제기 : 필요없는 추상클래스를 제거 하려 한다. 해결방법 : 상위 클래스와의 의존성을 제거*/
 	private static int INITIAL_CAPACITY = 10;
 	private Object[] elements = new Object[INITIAL_CAPACITY];
 	private int size = 0;
@@ -11,7 +11,9 @@ public class Set extends AbstractSet {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
+		if (!readOnly){
+
+
 			int newSize = size + 1;
 			if (newSize > elements.length) {
 				Object[] newElements =
