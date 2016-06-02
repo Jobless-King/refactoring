44425abfca1c56e02a7d1d85323edd7f3a461a3c
Ksj7 <lobstar733@gmail.com>
2016-06-02 12:14:15 +0900

    REFACTORING_v3
    interface를 사용하는 부분이 하나의 클래스 밖에 없어서 해당 인터페이스와 클래스를 합쳐서 단순화 했다.

--- src/com/nts/cleancode/collections/AbstractCollection.java
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -1,6 +1,15 @@
 package com.nts.cleancode.collections;
 
-public abstract class AbstractCollection implements Collection {
+public abstract class AbstractCollection  {/*문제제기 : interface를 사용하는 부분이 해당 클래스 밖에 없다 . 해결 : 인터페이스를 삭제하고 추상 메소드를 내부에 선언*/
+
+	/*collection interface의 값을 가지고 옮*/
+	abstract  protected boolean isEmpty();
+	abstract  protected void add(Object element);
+	abstract  protected boolean remove(Object element);
+	abstract  protected boolean contains(Object element);
+	abstract  protected int size();
+
+
 	public void addAll(AbstractCollection c) {
 		if (c instanceof Set) {
 			Set s = (Set)c;
@@ -9,7 +18,7 @@ public abstract class AbstractCollection implements Collection {
 					add(s.getElementAt(i));
 				}
 			}
-			
+
 		} else if (c instanceof List) {
 			List l = (List)c;
 			for (int i=0; i < l.size(); i++) {
@@ -19,11 +28,11 @@ public abstract class AbstractCollection implements Collection {
 			}
 		}else if (c instanceof Map) {
 			Map m = (Map)c;
-			for (int i=0; i<m.size(); i++) 
-				add(m.keys[i], m.values[i]);			
+			for (int i=0; i<m.size(); i++)
+				add(m.keys[i], m.values[i]);
 		}
 	}
-	
+
 	public void add(Object key, Object value) {
 	}
 }
