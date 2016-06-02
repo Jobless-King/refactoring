b4dbb94251d79c8d3c992786f7b28ce4c3a37822
Ksj7 <lobstar733@gmail.com>
2016-06-02 13:17:56 +0900

    REFACTORING_v5_1
    list의 잔해를 삭제함(intellij 자동삭제 옵션 미체크)

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -2,40 +2,6 @@ package com.nts.cleancode.collections;
 
 public class List extends AbstractCollection{ /* 문제제기 : 필요없는 추상클래스를 제거 하려 한다. 해결방법 : 상위 클래스와의 의존성을 제거*/
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
-
-	public boolean contains(Object element) {
-		for (int i=0; i<size; i++)
-			if (get(i).equals(element))
-				return true;
-		return false;
-	}
-
-	public boolean remove(Object element) {
-		if (readOnly)
-			return false;
-		else 	
-			for (int i = 0; i < size; i++)
-				if (get(i).equals(element)) {
-					elements[i] = null;
-					Object[] newElements = new Object[size - 1];
-					int k = 0;
-					for (int j = 0; j < size; j++) {
-						if (get(j) != null)
-							newElements[k++] = get(j);
-					}
-					size--;
-					elements = newElements;
-					return true;
-				}
-		return false;
-	}
-
-	public int capacity() {
-		return elements.length;
-	}
 
 	public void set(int i, Object value) {
 		if (readOnly)/*  조건문 안에 오독하기 쉬운 !표현 제거했다*/
@@ -44,9 +10,5 @@ public class List extends AbstractCollection{ /* 문제제기 : 필요없는 추
 			throw new ArrayIndexOutOfBoundsException();
 		elements[i] = value;
 
-	}
-
-	public void setReadOnly(boolean b) {
-		readOnly = b;
 	}
 }
